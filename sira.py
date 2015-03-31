f251412424141b5efc940fd47ef47c5f08237394
Gino <ginozhao2010@gmail.com>
2015-03-31 14:13:54 +0800

    optimise output result

--- sira.py
+++ sira.py
@@ -28,17 +28,18 @@ def sort_all_ip():
         fo.seek(0)
         lines = fo.readlines()
         for num in match_lines:
+            # latency less than 1S
             latency = re.findall(r'0.\d+', lines[num - 2])
-            if latency is not []:
+            if latency:
                 try:
                     match_ips[re.findall(r'[0-9]+(?:\.[0-9]+){3}', lines[num - 3])[0]] = float(latency[0])
                 except:
-                    print('line %s wrong' % num)
+                    print('line %s error!' % num)
 
     return sorted(match_ips.items(), key=operator.itemgetter(1))
 
 
-#get address map ip
+# get address map ip
 def reverse_address(rest_num, sorted_ips):
     fot = open('timeout', 'w')
     fca = open('collect_list', 'w')
@@ -49,6 +50,8 @@ def reverse_address(rest_num, sorted_ips):
         for line in fa:
             list_add.append(line.rstrip())
     list_temp = list_add[:]
+
+    set_add = set()
 
     for item in sorted_ips:
         try:
@@ -63,11 +66,13 @@ def reverse_address(rest_num, sorted_ips):
             else:
                 rev_add_temp.append(message[message.find('match') + 7:-1])
             # just collect site address
+            set_add = set_add.union(set(rev_add_temp))
             fca.write('ip:{} address:{} \n'.format(add_ip, str(rev_add_temp)))
             list_add = list_temp[:]
             for str_temp in list_add:
                 if str_temp in rev_add_temp:
-                    output.append('address=/{}/{}\n'.format(str_temp[2:] if str_temp.startswith('*.') else str_temp, add_ip))
+                    output.append(
+                        'address=/{}/{}\n'.format(str_temp[2:] if str_temp.startswith('*.') else str_temp, add_ip))
                     list_temp.remove(str_temp)
 
             print('{} is checked'.format(add_ip))
@@ -79,12 +84,27 @@ def reverse_address(rest_num, sorted_ips):
             print('{} is error, message:{}'.format(add_ip, e.message))
 
         rest_num -= 1
-        print('left {}'.format(str(rest_num)))
+        if rest_num > 0:
+            print('left {} item(s) will be check.'.format(str(rest_num)))
+        else:
+            print('left {} address(s) need to check.'.format(len(list_temp)))
 
         if not list_temp or not rest_num:
             break
 
+    if list_temp:
+        print('Notice: %s not found ip' % str(list_temp))
+    else:
+        print('Total {} items have been checked'.format(0 - rest_num if rest_num < 0 else rest_num))
     fot.close()
+
+    #output distinct address
+    collect_list = list(set_add)
+    collect_list.sort()
+    for item in collect_list:
+        fca.write(item + '\n')
+    fca.flush()
+    fca.close()
 
     ffd = open('dnsmasq', 'w')
     output.sort()
